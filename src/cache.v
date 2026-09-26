cb87a96fcc57d5a690b050662841cacba11c7ec3
JOSHUA5227 <joshua.quinthino@gmail.com>
2026-09-26 17:33:25 +0000

    Implementing WriteBack

--- src/cache.v
+++ src/cache.v
@@ -33,15 +33,18 @@ localparam OFFSET_BITS = $clog2(WORDS_PER);
 localparam INDEX_BITS = $clog2(CACHE_LINES);
 localparam TAG_BITS = ADDR_WIDTH - OFFSET_BITS - INDEX_BITS;
 
+reg [ADDR_WIDTH-1:0] writeback_base;
 reg [ADDR_WIDTH-1:0] refill_base;
 reg [ADDR_WIDTH-1:0] reg_addr;
 reg [DATA_WIDTH-1:0] reg_wdata;
 reg reg_read_write;
 
+reg dirty_bit_array [0:CACHE_LINES-1];
 reg valid_array [0:CACHE_LINES-1];
 reg [TAG_BITS-1:0] tag_array [0:CACHE_LINES-1];
 reg [DATA_WIDTH-1:0] data_array [0:CACHE_LINES-1][0:WORDS_PER-1]; 
 
+reg [COUNT_BITS-1:0] writeback_req_count;
 reg [COUNT_BITS-1:0] refill_req_count;
 reg [COUNT_BITS-1:0] refill_resp_count;
 
@@ -64,8 +67,9 @@ assign refill_resp_index = refill_resp_count[OFFSET_BITS-1:0];
 localparam IDLE = 3'd0;
 localparam LOOKUP = 3'd1;
 localparam FIFO_WAIT = 3'd2;
-localparam REFILL_WAIT = 3'd3;
-localparam RESPONSE = 3'd4;
+localparam WRITEBACK = 3'd3;
+localparam REFILL_WAIT = 3'd4;
+localparam RESPONSE = 3'd5;
 
 
 reg [2:0] present_state,next_state;
@@ -87,9 +91,9 @@ begin
     begin
              if(cache_hit)
              begin
-                if(reg_read_write)
-                 next_state = (!fifo_req_full) ? RESPONSE : FIFO_WAIT;
-                else
+             //   if(reg_read_write)
+             //   next_state = (!fifo_req_full) ? RESPONSE : FIFO_WAIT;
+             //   else implementing writeback
                  next_state = RESPONSE;
              end
              else
@@ -97,10 +101,17 @@ begin
                 if(reg_read_write)
                  next_state = (!fifo_req_full) ? RESPONSE : FIFO_WAIT;
                 else
-                 next_state = REFILL_WAIT;
+                begin
+                 if(dirty_bit_array[addr_index] == 1)
+                    next_state = WRITEBACK;
+                 else
+                    next_state = REFILL_WAIT;
+                end
              end 
     end
     FIFO_WAIT: next_state = (fifo_req_full) ? FIFO_WAIT : RESPONSE;
+    
+    WRITEBACK: next_state = (writeback_req_count == WORDS_PER)? REFILL_WAIT : WRITEBACK;
 
     REFILL_WAIT: next_state = ( (refill_req_count == WORDS_PER) && (refill_resp_count == WORDS_PER) ) ? RESPONSE : REFILL_WAIT;
 
@@ -122,6 +133,15 @@ begin
     fifo_resp_en =0;
 
     case(present_state)
+    WRITEBACK:
+    begin
+      if(!fifo_req_full && writeback_req_count < WORDS_PER )
+      begin
+        fifo_req_data = {1'b1,writeback_base+writeback_req_count,data_array[addr_index][writeback_req_count]};
+        fifo_req_en = 1;
+      end
+    end
+
     REFILL_WAIT:
     begin
       if(!fifo_req_full && refill_req_count < WORDS_PER )
@@ -132,7 +152,6 @@ begin
 
       if(!fifo_resp_empty && refill_resp_count <WORDS_PER && !resp_pending) 
          fifo_resp_en = 1;
-
     end
     
     RESPONSE:
@@ -141,8 +160,11 @@ begin
       
       if(reg_read_write)
       begin
-        fifo_req_data = {1'b1,reg_addr,reg_wdata};
-        fifo_req_en = 1;
+        if(!cache_hit && !fifo_req_full)
+        begin
+          fifo_req_data = {1'b1,reg_addr,reg_wdata}; 
+          fifo_req_en = 1;
+        end
       end
       else
         rdata = data_array[addr_index][addr_offset];
@@ -170,6 +192,7 @@ begin
     reg_wdata <=0;
     reg_read_write <= 0;
     refill_base <= 0;
+    writeback_base <= 0;
   end
   else
   begin
@@ -180,6 +203,10 @@ begin
       reg_read_write <= read_write;
       refill_base <= {addr[ADDR_WIDTH-1:OFFSET_BITS],{OFFSET_BITS{1'b0}} };
     end
+    else if(present_state == LOOKUP && !cache_hit && !reg_read_write && dirty_bit_array[addr_index])
+    begin
+        writeback_base <= {tag_array[addr_index],addr_index,{OFFSET_BITS{1'b0}} };
+    end
   end
 end
 
@@ -187,12 +214,18 @@ always@(posedge clk or negedge rst_n)
 begin
   if(!rst_n)
   begin
+    writeback_req_count <= 0;
     refill_req_count <= 0;
     refill_resp_count <= 0;
   end
   else
   begin
-      if(present_state == REFILL_WAIT)
+      if(present_state == WRITEBACK)
+      begin
+         if(!fifo_req_full && writeback_req_count < WORDS_PER)
+              writeback_req_count <= writeback_req_count + 1;
+      end
+      else if(present_state == REFILL_WAIT)
       begin
           if(!fifo_req_full && refill_req_count < WORDS_PER)
             refill_req_count <= refill_req_count + 1;
@@ -203,6 +236,7 @@ begin
       end
       else if(present_state == IDLE)
       begin
+          writeback_req_count <= 0;
           refill_req_count <= 0;
           refill_resp_count <= 0;
       end
@@ -216,10 +250,46 @@ begin
   if(!rst_n)
   begin
       for(i =0; i < CACHE_LINES; i = i+1)
+      begin
           valid_array[i] <= 0;
+          dirty_bit_array[i] <= 0;
+      end
   end
   else
   begin
+    case(present_state)
+    LOOKUP:
+    begin
+      if(cache_hit && reg_read_write)
+           data_array[addr_index][addr_offset] <= reg_wdata;
+    end
+    WRITEBACK:
+    begin
+      if(!fifo_req_full && (writeback_req_count == WORDS_PER-1))
+          dirty_bit_array[addr_index] <= 0;
+    end
+    REFILL_WAIT:
+    begin
+      if(resp_pending && refill_resp_count < WORDS_PER)
+      begin
+          data_array[addr_index][refill_resp_index] <= fifo_resp_data;
+          if(refill_resp_count == WORDS_PER-1)
+          begin
+              valid_array[addr_index] <= 1'b1;
+              tag_array[addr_index]   <= addr_tag;
+          end
+      end
+    end
+    RESPONSE:
+    begin
+       if(reg_read_write)
+       begin
+          dirty_bit_array[addr_index] <= 1'b1;
+          data_array[addr_index][addr_offset] <= reg_wdata;
+       end
+    end
+    endcase
+    /*
       if(present_state == LOOKUP && cache_hit && reg_read_write)
         data_array[addr_index][addr_offset] <= reg_wdata;
 
@@ -231,6 +301,7 @@ begin
             valid_array[addr_index] <= 1'b1;
             tag_array[addr_index]   <= addr_tag;
         end
+      */
   end
 end
 
